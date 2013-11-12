990dc9a1b81c07e6d307046e7044323ee30bbe54
emre <eyucel@server.fake>
2013-11-12 10:47:11 -0600

    changed calculation of switches

--- pl3.r
+++ pl3.r
@@ -143,15 +143,19 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
     s[,9]  = so[,9] + xs*xs1
     s[,10]  = so[,10] + 1
     
-    zero.index = zo == 0
-    one.index = zo == 1
+    zero.index = zs == 0
+    one.index = zs == 1
     
-
-    s[zero.index,12] = so[zero.index,12]+zo[zero.index]-zs1[zero.index]+1
-    s[zero.index,13] = so[zero.index,13]+zs1[zero.index]
-    s[one.index,14] = so[one.index,14]+zo[one.index]-zs1[one.index]
-    s[one.index,15] = so[one.index,15]+zs1[one.index]-zo[one.index]+1
-    
+    zero.index.2 = zs1 == 0
+    one.index.2 = zs1 == 1
+#     s[zero.index,12] = so[zero.index,12]+zo[zero.index]-zs1[zero.index]+1
+#     s[zero.index,13] = so[zero.index,13]+zs1[zero.index]
+#     s[one.index,14] = so[one.index,14]+zo[one.index]-zs1[one.index]
+#     s[one.index,15] = so[one.index,15]+zs1[one.index]-zo[one.index]+1
+    s[,12] = so[,12]+zero.index*zero.index.2
+    s[,13] = so[,13]+zero.index*one.index.2
+    s[,14] = so[,14]+one.index*zero.index.2
+    s[,15] = so[,15]+one.index*one.index.2
 #     print(s[,12:15])
     blk.A = s[,4]*s[,6] - s[,5]^2
     blk.B = -(s[,2]*s[,6]-s[,3]*s[,5])
@@ -460,7 +464,7 @@ PL = function(y,alphas,betas,tau2s,ps,qs,xs,zs){
 
 # Simulated data
 # set.seed(98765)
-n     =  200
+n     =  1000
 alpha =  -1
 gamma = 1
 beta  =  0.5
