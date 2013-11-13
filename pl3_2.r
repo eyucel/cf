bc4ae4e025130f27477eaf7b92f7e5f648937774
Emre <emyucel@live.com>
2013-11-12 18:46:53 -0600

    closest to working that I will get

--- pl3_2.r
+++ pl3_2.r
@@ -12,8 +12,8 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
                   N,nmix,byrow=TRUE) 
   
   quants = array(0,c(n,8,3))
-  z      = log(y^2)
-  #   z = y
+    z      = log(y^2)
+  
   s      = matrix(0,N,15)
   zmean=rep(0,n)
   
@@ -28,10 +28,10 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
   s[,9] = s[,3]*b0[1]+s[,5]*b0[2]+s[,6]*b0[3]
   s[,10] = c0
   s[,11] = d0
-  s[,12] = 1
-  s[,13] = 1
-  s[,14] = 1
-  s[,15] = 1
+  s[,12] = 0
+  s[,13] = 0
+  s[,14] = 0
+  s[,15] = 0
   blk.A = s[,4]*s[,6] - s[,5]^2
   blk.B = -(s[,2]*s[,6]-s[,3]*s[,5])
   blk.C = s[,2]*s[,5]-s[,3]*s[,5]
@@ -46,56 +46,40 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
   b1   = (s[,7]*blk.A + s[,8] * blk.B + s[,9] * blk.C)/m
   b2   = (s[,7]*blk.B + s[,8] * blk.E + s[,9] * blk.F)/m
   b3   = (s[,7]*blk.C + s[,8] * blk.F + s[,9] * blk.I)/m
-  zs1 = zs
+  
   for (t in 1:n){
-    
-    zo = zs1
-    
-#     print(zo)
-    zero.index = zo == 0
-    one.index = zo == 1
-#     ll = sum(zero.index)
-#     print(zero.index)
-#     print(1-ps[zero.index])
-#     print(qs[one.index])
-#     zs[zero.index]     = rbinom(ll,1,1-ps[zero.index])
-#     zs[one.index]      = rbinom(N-ll,1,qs[one.index])
-    
-
-#     zs = (apply(cbind(ps,1-ps),1,which.max)-1)*zero.index
-#     zs = zs+(apply(cbind(1-qs,qs),1,which.max)-1)*one.index
-    zs = rbinom(N,1,1-ps)*zero.index    
-    zs = zs+rbinom(N,1,qs)*one.index
-#     print(zs)
+#     print(t)
     #print(t)
-#     mus1    = matrix(alphas+gammas+betas*xs,N,nmix)
-#     mus    = matrix(alphas+betas*xs,N,nmix)
-    mus = matrix(alphas+gammas*zs+betas*xs,N,nmix)
-#     print(mus)
+    mus1    = matrix(alphas+gammas+betas*xs,N,nmix)
+    mus    = matrix(alphas+betas*xs,N,nmix)
     #     print(mus[1,])
     #     scan(n=1)
-        probs  = q*dnorm(z[t],mus+mu,sqrt(sig2+tau2s))
-#     switch1 = qs*zs + (1-zs)*(1-ps)
-#     switch0 = 1-switch1 #(1-zs)*ps + zs*(1-qs)
-#     left = q*(dnorm(z[t],mus1+mu,sqrt(sig2+tau2s)))
-#     right = q*(dnorm(z[t],mus+mu,sqrt(sig2+tau2s)))
-#     probs = left*switch1+right*switch0
-#     lefts = apply(left,1,sum)
-#     rights = apply(right,1,sum)
-#     weight = lefts*switch1+rights*switch0
-        weight = apply(probs,1,sum)
+    #     probs  = q*dnorm(z[t],mus+mu,sqrt(sig2+tau2s))
     
-    #     scan(n=1)
+    zero.index= zs == 0
+    one.index = zs == 1
+    
+    
+    switch1 = qs*one.index + zero.index*(1-ps)
+    switch0 = 1-switch1 #(1-zs)*ps + zs*(1-qs)
+    left = q*(dnorm(z[t],mus1+mu,sqrt(sig2+tau2s)))
+    right = q*(dnorm(z[t],mus+mu,sqrt(sig2+tau2s)))
+    probs = left*switch1+right*switch0
+    lefts = apply(left,1,sum)
+    rights = apply(right,1,sum)
+    weight = lefts*switch1+rights*switch0
+    
+#     probs = q*(dnorm(z[t],mus+mu,sqrt(sig2+tau2s))*(qs*zs+(1-zs)*(1-ps)) + dnorm(z[t],mus1+mu,sqrt(sig2+tau2s))*((1-zs)*ps+zs*(1-qs)))
+    
+#     weight = apply(probs,1,sum)
     k      = sample(1:N,size=N,replace=TRUE,prob=weight)
     #     print(probs[1,])
     #     scan(n=1)
     
     xs1    = xs[k]
     zs1    = zs[k]
-#     print(zs1)
     probs  = probs[k,]
     mus    = mus[k,]
-#     mus1   = mus1[k,]
     alphas = alphas[k]
     gammas = gammas[k]
     betas  = betas[k]
@@ -108,59 +92,70 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
     b3o  = b3[k]
     
     
-
     
     
-#     switch1 = qs*zs1 + (1-zs1)*(1-ps)
-#     switch0 = 1-switch1 #(1-zs1)*ps + zs1*(1-qs)
-    
-    #     zs = zz[t+1,]
-#     pp = lefts[k]*switch1/weight[k]
-#     qq = rights[k]*switch0/weight[k]
     
     tau2ss = matrix(tau2s,N,nmix)
     vars   = 1/(1/sig2+1/tau2ss)
     sds    = sqrt(vars)
     
+    zero.index = zs1 == 0
+    one.index  = zs1 == 1
+    
+#     mus    = matrix(alphas+gammas+betas*xs1,N,nmix)
+#     mus1    = matrix(alphas+betas*xs1,N,nmix)
     
     
-  #     zs = rbinom(N,1,1-ps)*zero.index    
-  #     zs = zs+rbinom(N,1,qs)*one.index    
-#     mus.temp = matrix(alphas+gammas*zs+betas*xs1,N,nmix)
-    mus = matrix(alphas+betas*xs1,N,nmix)
-    means  = vars*((z[t]-mu-gammas*zs1)/sig2 + mus/tau2ss)
+#     switch1 = qs*one.index + zero.index*(1-ps)
+#     switch0 = 1-switch1 #(1-zs)*ps + zs*(1-qs)
+#     left = q*(dnorm(z[t],mus1+mu,sqrt(sig2+tau2s)))
+#     right = q*(dnorm(z[t],mus+mu,sqrt(sig2+tau2s)))
+#     probs = left*switch1+right*switch0
+#     lefts = apply(left,1,sum)
+#     rights = apply(right,1,sum)
+#     weight = lefts*switch1+rights*switch0
+#     
+#     pp = lefts*switch1/weight
+#     qq = rights*switch0/weight
+    
+    
+    zs     = rbinom(N,1,1-ps)*zero.index
+    zs    = zs+ rbinom(N,1,qs)*one.index
+    
+    mus = matrix(alphas+gammas*zs+betas*xs1,N,nmix)
+    probs = q*(dnorm(z[t],mus+mu,sqrt(sig2+tau2s)))
+    means  = vars*((z[t]-mu)/sig2 + mus/tau2ss)
     for (i in 1:N){
       comp  = sample(1:nmix,size=1,prob=probs[i,])
       xs[i] = rnorm(1,means[i,comp],sds[i,comp])
     }
     
+    zero.index = zs1 == 0
+    one.index = zs1 == 1
+    zero.index.2 = zs == 0
+    one.index.2 = zs == 1
     
-    
+    #     zs = zz[t+1,]
+
     s[,1]  = so[,1] + 1
-    s[,2]  = so[,2] + zs1
+    s[,2]  = so[,2] + zs
     s[,3]  = so[,3] + xs1
-    s[,4]  = so[,4] + zs1^2
-    s[,5]  = so[,5] + zs1*xs1
+    s[,4]  = so[,4] + zs^2
+    s[,5]  = so[,5] + zs*xs1
     s[,6]  = so[,6] + xs1^2
     s[,7]  = so[,7] + xs
-    s[,8]  = so[,8] + xs*zs1
+    s[,8]  = so[,8] + xs*zs
     s[,9]  = so[,9] + xs*xs1
     s[,10]  = so[,10] + 1
     
-
-    zero.index = zo == 0
-    one.index = zo == 1    
-    zero.index.2 = zs1 == 0
-    one.index.2 = zs1 == 1
-#     s[zero.index,12] = so[zero.index,12]+zo[zero.index]-zs1[zero.index]+1
-#     s[zero.index,13] = so[zero.index,13]+zs1[zero.index]
-#     s[one.index,14] = so[one.index,14]+zo[one.index]-zs1[one.index]
-#     s[one.index,15] = so[one.index,15]+zs1[one.index]-zo[one.index]+1
+#     s[zero.index,12] = so[zero.index,12]+zs1[zero.index]-zs[zero.index]+1
+#     s[zero.index,13] = so[zero.index,13]+zs[zero.index]
+#     s[one.index,14] = so[one.index,14]+zs1[one.index]-zs[one.index]
+#     s[one.index,15] = so[one.index,15]+zs[one.index]-zs1[one.index]+1
     s[,12] = so[,12]+zero.index*zero.index.2
     s[,13] = so[,13]+zero.index*one.index.2
     s[,14] = so[,14]+one.index*zero.index.2
     s[,15] = so[,15]+one.index*one.index.2
-#     print(s[,12:15])
     blk.A = s[,4]*s[,6] - s[,5]^2
     blk.B = -(s[,2]*s[,6]-s[,3]*s[,5])
     blk.C = s[,2]*s[,5]-s[,3]*s[,4]
@@ -180,7 +175,7 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
     b2   = (s[,7]*blk.B + s[,8] * blk.E + s[,9] * blk.F)/m
     b3   = (s[,7]*blk.C + s[,8] * blk.F + s[,9] * blk.I)/m
     
-    s[,11]  = so[,11] + (xs-b1-b2*zs1-b3*xs1)*xs + (b1o-b1)*so[,7]+(b2o-b2)*so[,8]+(b3o-b3)*so[,9]
+    s[,11]  = so[,11] + (xs-b1-b2*zs-b3*xs1)*xs + (b1o-b1)*so[,7]+(b2o-b2)*so[,8]+(b3o-b3)*so[,9]
     
     tau2s  = 1/rgamma(N,s[,10]/2,s[,11]/2)
     std    = sqrt(tau2s/m)
@@ -209,9 +204,7 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
     
     ps = rbeta(N,s[,12]+1,s[,13]+1)
     qs = rbeta(N,s[,15]+1,s[,14]+1)
-#     print(ps)
-#     print(qs)
-#     scan(n=1)
+    
     # Storing quantiles
     quants[t,1,] = quantile(alphas,c(0.025,0.5,0.975))
     quants[t,2,] = quantile(gammas,c(0.025,0.5,0.975))
@@ -219,9 +212,9 @@ svm.pl = function(y,alphas,betas,tau2s,ps,qs,xs,zs){#,b0,B0,c0,d0){
     quants[t,4,] = quantile(tau2s,c(0.025,0.5,0.975))  
     quants[t,5,] = quantile(ps,c(0.025,0.5,0.975))  
     quants[t,6,] = quantile(qs,c(0.025,0.5,0.975))  
-    quants[t,7,] = quantile(zs1,c(0.025,0.5,0.975))
+    quants[t,7,] = quantile(zs,c(0.025,0.5,0.975))
     quants[t,8,] = quantile(exp(xs/2),c(0.025,0.5,0.975))
-    zmean[t] =  mean(zs1)
+    zmean[t] =  mean(zs)
   }
   return(list(quants=quants,zmean=zmean))
 }
@@ -274,16 +267,15 @@ PL = function(y,alphas,betas,tau2s,ps,qs,xs,zs){
   for (t in 1:n){
     #     zs = zz[t,]
     # Resampling
-    mus    = alphas+gammas+betas*xs
+    mus    = alphas+gammas*zs+betas*xs
     stdevs = exp(mus/2)
     stdevs1 = exp((alphas+betas*xs)/2)
-    weight = dnorm(y[t],0,stdevs)*(qs+(1-zs)*(1-ps)) + dnorm(y[t],0,stdevs1)*((1-zs)*ps+zs*(1-qs))
-    #     weight = dnorm(y[t],0,stdevs)
+    #     weight = dnorm(y[t],0,stdevs)*(qs+(1-zs)*(1-qs)) + dnorm(y[t],0,stdevs1)*((1-zs)*ps+zs*(1-ps))
+    weight = dnorm(y[t],0,stdevs)
     k      = sample(1:N,size=N,replace=T,prob=weight)
     alphas = alphas[k]
     betas  = betas[k]
     tau2s  = tau2s[k]
-    gammas = gammas[k]
     ps = ps[k]
     qs = qs[k]
     
@@ -302,9 +294,8 @@ PL = function(y,alphas,betas,tau2s,ps,qs,xs,zs){
     zs[zero.index]     = rbinom(ll,1,1-ps[zero.index])
     zs[one.index]      = rbinom(N-ll,1,qs[one.index])
     #     mus    = alphas+gammas*zs+betas*xs1
-    xs     = rnorm(N,means,sds)
-    xs = alphas+betas*xs + gammas*zs + rnorm(N,0,sqrt(tau2s))
-    
+    #     xs     =  rnorm(N,means,sds)
+    xs = mus + rnorm(N,0,sqrt(tau2s))
     #     print(zs)
     #     print(qs)
     #     print(ps)
@@ -447,8 +438,8 @@ PL = function(y,alphas,betas,tau2s,ps,qs,xs,zs){
     #     gammas = rtnorm(N,mean=b2,lower=0)
     betas  = b3 + L31*norm[,1] + L32 * norm[,2] + L33 * norm[,3]
     
-    #     gamma.mean = b2 - 1/s[,4]*(s[,2]*(alphas-b1)+s[,5]*(betas-b3))
-    #     gammas = rtnorm(N,gamma.mean,sd = sqrt(tau2s/s[,4]),lower=0)
+    gamma.mean = b2 - 1/s[,4]*(s[,2]*(alphas-b1)+s[,5]*(betas-b3))
+    gammas = rtnorm(N,gamma.mean,sd = sqrt(tau2s/s[,4]),lower=0)
     
     ps = rbeta(N,s[,12]+1,s[,13]+1)
     qs = rbeta(N,s[,15]+1,s[,14]+1)
@@ -468,34 +459,33 @@ PL = function(y,alphas,betas,tau2s,ps,qs,xs,zs){
 
 # Simulated data
 # set.seed(98765)
-n     =  200
+n     =  300
 alpha =  -1
 gamma = 1.5
 beta  =  0.5
 tau2  =  .3
-#sig2  =  1.0
+sig2  =  1.0
 tau   = sqrt(tau2)
 p0 = .99
 q0 = .985
 
 x     = rep(alpha/(1-beta),n+1)
 S = rep(0,n+1)
-S[1] = rbinom(1,1,p0/(1-q0)/100)
 true  = c(alpha,gamma,beta,tau2,p0,q0)
 names = c("alpha","gamma","beta","tau2","p","q")
 
-# S = c(1,1,1,1,1,0,0,0,0,0,0)
+
 for (t in 2:(n+1))
-{
+  {
   if (S[t-1] == 0)
     S[t] = rbinom(1,1,1-p0)
   if (S[t-1] == 1)
     S[t] = rbinom(1,1,q0)
-  
+
   x[t] = alpha+gamma*S[t]+beta*x[t-1]+tau*rnorm(1)}
 x = x[2:(n+1)]
 y = rnorm(n,0,exp(x/2))
-print(S)
+# print(S)
 par(mfrow=c(1,1))
 plot(y,ylim=range(x,y),xlab="Time",ylab="",main="",pch=16)
 lines(x,col=2,lwd=2)
@@ -547,23 +537,27 @@ plm = out$quants
 
 print(date())
 cols = c(grey(0.5),1,grey(0.5))
-ind  = 1:length(y)
+ind  = 20:length(y)
 n1   = length(ind)
 par(mfrow=c(3,2))
 for (i in 1:6){
   ts.plot(plm[ind,i,],xlab="",ylab="",main=names[i],col=cols,ylim=range(plm[ind,i,]))
   abline(h=true[i],lty=2)
 }
+
+
 par(mfrow=c(1,1))
-# ts.plot(plm[ind,7,],xlab="",ylab="",main=names[i],col=cols,ylim=range(plm[ind,i,]))
-# par(mfrow=c(1,1))
+
+par(mfrow=c(1,1))
+ts.plot(plm[,7,2],xlab="",ylab="",main="states",col=1,ylim=c(-1,2),lwd=2)
+lines(S[2:(n+1)],col='blue',ylim=c(-1,2),lwd=2,lty=2)
 # ts.plot(cbind(2*plm[,8,2],-2*plm[,8,2]))
 # lines(returns*100,col='red')
 
 # ts.plot(out$zmean)
-par(mfrow=c(1,1))
-ts.plot(plm[,7,2],xlab="",ylab="",main="states",col=1,ylim=c(-1,2),lwd=2)
-lines(S[2:(n+1)],col='red',ylim=c(-1,2),lwd=1)
+# par(mfrow=c(1,1))
+# ts.plot(plm[,7,2],xlab="",ylab="",main="states",col=1,ylim=c(-1,2),lwd=2)
+# points(S[2:(n+1)],col='red',ylim=c(-1,2),lwd=1)
 
 
 # Particle filters
